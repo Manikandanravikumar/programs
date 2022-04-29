9ca389e8f3368e788178c19b0b4b930c313b8191
Manikandanravikumar <101857943+Manikandanravikumar@users.noreply.github.com>
2022-04-29 21:04:49 +0530

    Create find the median.java

---
+++ find the median.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(s[i]);
+        }
+        Arrays.sort(a);
+        System.out.println(a[n/2]);
+    
+    }
+}
