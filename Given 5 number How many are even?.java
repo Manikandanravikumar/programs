f94b54a14812d2d8004036ff1c32a80934a5aff5
Manikandanravikumar <101857943+Manikandanravikumar@users.noreply.github.com>
2022-05-24 15:52:50 +0530

    Create Given 5 number How many are even?.java

---
+++ Given 5 number How many are even?.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int input1=sc.nextInt();
+        int input2=sc.nextInt();
+        int input3=sc.nextInt();
+        int input4=sc.nextInt();
+        int input5=sc.nextInt();
+        int c=0;
+		if(input1%2==0)
+		{
+			c++;
+		}
+		if(input2%2==0)
+		{
+			c++;
+		}
+		if(input3%2==0)
+		{
+			c++;
+		}
+		if(input4%2==0)
+		{
+			c++;
+		}
+		if(input5%2==0)
+		{
+			c++;
+		}
+        System.out.println(c);
+    }
+}
