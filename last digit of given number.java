73af5a561fa02f3f4b925305c998a0a247ecf724
Manikandanravikumar <101857943+Manikandanravikumar@users.noreply.github.com>
2022-05-24 15:35:58 +0530

    Create last digit of given number.java

---
+++ last digit of given number.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        int input1 = 197;
+        int n =input1%10;
+        System.out.println(n);
+    }
+}
