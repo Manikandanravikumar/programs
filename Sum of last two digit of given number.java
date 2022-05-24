d85c2fb76392daae9c983df8f1d4ef4026f2ec40
Manikandanravikumar <101857943+Manikandanravikumar@users.noreply.github.com>
2022-05-24 14:21:55 +0530

    Create Sum of last two digit of given number.java

---
+++ Sum of last two digit of given number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        int input1 = 197;
+        int input2 = 194;
+        int n=input1%10;
+        int n1=input2%10;
+        System.out.println(n+n1);
+    }
+}
